461277f382368717aef642368c74de95d907b27f
Morten Holt <mho@delegate.dk>
2022-02-02 23:06:53 +0100

    Do not remove index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-export * from './lib';
-export * from './visitors';
-export * from './pluginConfig';
+import ts from "typescript";
+
+import { paramsTypeNameString } from "@/lib";
+import FilePrinter from "@/lib/filePrinter";
+import { visitor } from "@/visitors";
+
+import { IPluginConfig } from "./pluginConfig";
+
+export default (opts: IPluginConfig) =>
+	(ctx: ts.TransformationContext) =>
+		(sourceFile: ts.SourceFile) => {
+			// Check: Source already has the type declared, if it does we've probably already handled this
+			const existingTypeDef = ts.forEachChild(sourceFile, (n) =>
+				ts.isTypeAliasDeclaration(n) && n.name.getText(sourceFile) === paramsTypeNameString
+					? n
+					: undefined
+			)
+			if (existingTypeDef) {
+				if (opts.verbose) console.log("Params type already declared, skipping " + sourceFile.fileName)
+				return sourceFile;
+			}
+
+			const updatedSource = ts.visitEachChild(sourceFile, visitor(sourceFile, opts, ctx), ctx)
+			if (updatedSource == sourceFile) return sourceFile
+
+			if (opts.printGenerated)
+				FilePrinter(sourceFile, updatedSource, opts)
+
+			return updatedSource
+		};
