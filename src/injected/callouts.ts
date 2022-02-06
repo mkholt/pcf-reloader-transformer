14853672f4cd85e021a0ad86a6bd3d8cc1daef77
Morten Holt <mho@delegate.dk>
2022-02-06 22:08:26 +0100

    Add tests for callout functions. Always disconnect from socket on reload call

--- src/injected/callouts.ts
+++ src/injected/callouts.ts
@@ -6,6 +6,7 @@ import {
 declare const window: PcfReloaderWindow
 
 const isScript = (s: HTMLOrSVGScriptElement): s is HTMLScriptElement => !!(s as HTMLScriptElement).src;
+export const hasParams = () => !!window.pcfReloadParams
 
 export type ComponentType = ComponentFramework.StandardControl<unknown, unknown>
 
@@ -15,14 +16,12 @@ export const onConstruct = <T extends ComponentType>(instance: T, currentScript:
 	_currentScript = currentScript
 	_instance = instance
 	
-	if (!window.pcfReloadParams) return
+	if (!hasParams()) return
 	
 	const params = window.pcfReloadParams;
 	instance.init(params.context, params.notifyOutputChanged, params.state, params.container);
 	instance.updateView(params.context);
 }
-
-export const hasParams = () => !!window.pcfReloadParams
 
 export const onUpdateContext = (context: ComponentFramework.Context<unknown>) => {
 	window.pcfReloadParams = {
@@ -32,19 +31,26 @@ export const onUpdateContext = (context: ComponentFramework.Context<unknown>) =>
 }
 
 export const reloadComponent = () => {
+	// A reload always results in a disconnect,
+	// if we are not in a valid state, there is no way to recover
+	doDisconnect()
+
+	// If we don't have an instance, abort
 	if (!_instance) return
 
-	_instance.destroy()
-	doDisconnect()
+	// If we don't have a currentScript, or it's not a script tag, abort
 	if (!_currentScript || !isScript(_currentScript))
 		return
 
+	// If the script doesn't have a parent, abort
+	const parent = _currentScript.parentNode
+	if (!parent) return
+	
+	// We are in a valid state, destroy and rebuild
+	_instance.destroy()
+
 	const script = document.createElement("script")
 	script.src = _currentScript.src
-
-	const parent = _currentScript.parentNode
-
-	if (!parent) return
 	
 	_currentScript.remove()
 
--- tests/injected/callout.test.ts
+++ tests/injected/callout.test.ts
@@ -6,17 +6,208 @@ import {
 	mock,
 	mockClear,
 	mockDeep,
+	mockReset,
 } from "jest-mock-extended";
 
 import * as injected from "../../src/injected";
+import { ComponentType } from "../../src/injected";
 
-describe('callout tests', () => {
-	it('reads window params', () => {
-		//expect({}).toBeNull()
+const init = () => {
+	const getParams = jest.fn()
+	const setParams = jest.fn()
+
+	const component = mock<ComponentType>({
+		init: jest.fn(),
+		updateView: jest.fn()
+	})
+
+	beforeAll(() => {
+		Object.defineProperty(global.window, "pcfReloadParams", {
+			configurable: true,
+			get: getParams,
+			set: setParams
+		})
+	})
+
+	beforeEach(() => {
+		jest.resetAllMocks()
+		mockReset(component)
+	})
+
+	return { getParams, setParams, component }
+}
+
+describe('onConstruct', () => {
+	const { getParams, component } = init()
+
+	it('calls init and updateView if params are set', () => {
+		getParams.mockReturnValue(mock<injected.ReloadParams>())		
+		injected.onConstruct(component, document.createElement("script"))
+
+		expect(component.init).toHaveBeenCalled()
+		expect(component.updateView).toHaveBeenCalled()
+	})
+
+	it('does not call init and updateView if params are not set', () => {
+		injected.onConstruct(component, document.createElement("script"))
+
+		expect(component.init).not.toHaveBeenCalled()
+		expect(component.updateView).not.toHaveBeenCalled()
 	})
 })
 
-describe('callout method calls', () => {
+describe('hasParams', () => {
+	const { getParams } = init()
+
+	it('returns hasParams if params are set', () => {
+		getParams.mockReturnValue(mock<injected.ReloadParams>())
+		expect(injected.hasParams()).toBeTruthy()
+
+		getParams.mockReturnValue(undefined)
+		expect(injected.hasParams()).toBeFalsy()
+	})
+})
+
+describe('onUpdateContext', () => {
+	const { getParams, setParams } = init()
+	it('updates the context in the params', () => {
+		const oldParams = {
+			container: document.createElement("div"),
+			context: mock<ComponentFramework.Context<unknown>>(),
+			notifyOutputChanged: jest.fn(),
+			state: mock<ComponentFramework.Dictionary>()
+		}
+		getParams.mockReturnValue(oldParams)
+
+		const newContext = mock<ComponentFramework.Context<unknown>>()
+
+		injected.onUpdateContext(newContext)
+		expect(getParams).toHaveBeenCalled()
+		expect(setParams).toHaveBeenCalledWith(expect.objectContaining<Partial<injected.ReloadParams>>({
+			context: newContext,
+			container: oldParams.container,
+			notifyOutputChanged: oldParams.notifyOutputChanged,
+			state: oldParams.state
+		}))
+	})
+})
+
+describe('reloadComponent', () => {
+	const getComponent = () => mock<ComponentType>({
+		init: jest.fn(),
+		updateView: jest.fn(),
+		destroy: jest.fn()
+	})
+
+	const doDisconnect = jest.fn()
+	beforeAll(() => {
+		jest.mock('../../src/injected/sync', () => ({
+			doDisconnect: doDisconnect
+		}))
+	})
+
+	beforeEach(() => {
+		jest.resetAllMocks()
+	})
+
+	it('reloads the component on reload', () => {
+		jest.isolateModules(() => {
+			const component = getComponent()
+
+			const scriptWrapper = document.createElement("div")
+			const scriptTag = document.createElement("script")
+			scriptTag.src = "http://example.com"
+			scriptTag.setAttribute("data-original", "yes")
+			const currentScript = scriptWrapper.appendChild(scriptTag)
+
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			const callouts = require('../../src/injected/callouts')
+			callouts.onConstruct(component, currentScript)
+			callouts.reloadComponent()
+
+			// Disconnect websocket
+			expect(doDisconnect).toHaveBeenCalled()
+
+			// Destroy the component
+			expect(component.destroy).toHaveBeenCalled()
+
+			// Script element is replaced
+			expect(scriptWrapper.innerHTML).toEqual('<script src="' + scriptTag.src + '"></script>')
+		})
+	})
+
+	it('aborts without instance', () => {
+		jest.isolateModules(() => {
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			const callouts = require('../../src/injected/callouts')
+			callouts.reloadComponent()
+
+			expect(doDisconnect).toHaveBeenCalled()
+		})
+	})
+
+	it('aborts without current script', () => {
+		jest.isolateModules(() => {
+			const component = getComponent()
+
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			const callouts = require('../../src/injected/callouts')
+			callouts.onConstruct(component, null)
+			callouts.reloadComponent()
+
+			expect(doDisconnect).toHaveBeenCalled()
+			expect(component.destroy).not.toHaveBeenCalled()
+		})
+	})
+
+	it('aborts when currentscript is not script tag', () => {
+		jest.isolateModules(() => {
+			const component = getComponent()
+
+			const scriptWrapper = document.createElement("div")
+			const scriptTag = document.createElementNS("http://www.w3.org/2000/svg", "script")
+			const currentScript = scriptWrapper.appendChild(scriptTag)
+
+			const scriptWrapperHTML = scriptWrapper.innerHTML
+
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			const callouts = require('../../src/injected/callouts')
+			callouts.onConstruct(component, currentScript)
+			callouts.reloadComponent()
+
+			// Disconnect websocket
+			expect(doDisconnect).toHaveBeenCalled()
+
+			// Do not abort, since we cannot rebuild
+			expect(component.destroy).not.toHaveBeenCalled()
+
+			// Script element is NOT replaced
+			expect(scriptWrapper.innerHTML).toEqual(scriptWrapperHTML)
+		})
+	})
+
+	it('aborts when script has no parent', () => {
+		jest.isolateModules(() => {
+			const component = getComponent()
+
+			const currentScript = document.createElement("script")
+			currentScript.src = "http://example.tld"
+
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			const callouts = require('../../src/injected/callouts')
+			callouts.onConstruct(component, currentScript)
+			callouts.reloadComponent()
+
+			// Disconnect websocket
+			expect(doDisconnect).toHaveBeenCalled()
+
+			// Destroy the component
+			expect(component.destroy).not.toHaveBeenCalled()
+		})
+	})
+})
+
+describe('updated code calls callout functions', () => {
 	let injectMock: typeof injected
 	const currentScript = document.createElement("script")
 
