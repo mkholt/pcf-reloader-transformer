7ddc5fc7b96909db5ba605c7b913dd168b604366
Morten Holt <mho@delegate.dk>
2022-07-06 17:17:29 +0200

    More thorough implementation of react control

--- src/injected/controls/reactControl.ts
+++ src/injected/controls/reactControl.ts
@@ -1,11 +1,20 @@
 import React from 'react';
 
-import ComponentWrapper from '../components/componentWrapper';
+import ComponentWrapper, {
+	ComponentMode,
+	ComponentWrapperProps,
+} from '../components/componentWrapper';
+import {
+	error,
+	log,
+} from '../logger';
 import { BaseControl } from './base';
 
 type ComponentType<IInputs, IOutputs> = ComponentFramework.ReactControl<IInputs, IOutputs>
 
 export class ReactControl<IInputs, IOutputs> extends BaseControl<ComponentType<IInputs, IOutputs>, IInputs> implements ComponentType<IInputs, IOutputs> {
+	private _componentRef: ComponentWrapper|null = null
+
 	constructor(className: string, baseUrl: string, script: HTMLOrSVGScriptElement|null, showForceReload: boolean) {
 		super(className, baseUrl, script, showForceReload)
 	}
@@ -17,20 +26,49 @@ export class ReactControl<IInputs, IOutputs> extends BaseControl<ComponentType<I
 		}
 
 		const component = this.wrapped?.updateView(context)
-		return React.createElement(ComponentWrapper, { component })
+		const props: ComponentWrapperProps = {
+			componentName: this.className,
+			component: component,
+			showForceReload: this.showForceReload,
+			onReload: () => this.reloadComponent()
+		}
+		return React.createElement(ComponentWrapper, {
+			...props,
+			ref: c => this._componentRef = c
+		})
 	}
 
 	protected onLoadScript(): void {
-		throw new Error("Method not implemented.");
+		log(`Replacing wrapped instance of ${this.className}`)
+
+		const wrapped = this.buildWrapped()
+		if (!this.params || !wrapped) return;
+		log(`Got new instance of component, initializing and building component`)
+
+		// The new instance was initialized, call it with the last known state of the component
+		const { context, notifyOutputChanged, state, container } = this.params
+		wrapped.init(context, notifyOutputChanged, state, container)
+		const component = wrapped.updateView(context)
+
+		// Set the state of the wrapper component to update the component and disable the spinner
+		this._componentRef?.setState({
+			component: component,
+			mode: ComponentMode.Component
+		})
 	}
 
-	protected onScriptError(): void {
-		throw new Error("Method not implemented.");
+	protected onScriptError(e: ErrorEvent): void {
+		error(`An error occurred loading the ${this.className} component:`, e.message)
+
+		this._componentRef?.setState({
+			mode: ComponentMode.Error
+		})		
 	}
 
 	protected wrapContainer(): undefined {
 		return undefined
 	}
+
 	protected showSpinner(): void {
 		// Tell the wrapper to show the spinner
 		this._componentRef?.setState({
